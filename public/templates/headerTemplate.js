740ecd8a1eab63483a7dc135f1433335409c1e9d
austin <wilmotha@oregonstate.edu>
2018-05-28 17:46:48 -0700

    CSS files should work

--- public/templates/headerTemplate.js
+++ public/templates/headerTemplate.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['headerTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "\n\n\n    <header>\n      <nav class=\"navbar\">\n          <h1 class=\"Mixer-Title\">Mixer</h1>\n          <p class=\"navbar-search\">\n            <input type=\"text\" id=\"navbar-search-input\" placeholder=\"Input here to search\">\n            <button type=\"button\" id=\"Mixer-Search\"></button>\n          </p>\n      </nav>\n    </header>\n\n\n";
+    return "\r\n\r\n\r\n    <header>\r\n      <nav class=\"navbar\">\r\n          <h1 class=\"Mixer-Title\">Mixer</h1>\r\n          <p class=\"navbar-search\">\r\n            <input type=\"text\" id=\"navbar-search-input\" placeholder=\"Input here to search\">\r\n            <button type=\"button\" id=\"Mixer-Search\"></button>\r\n          </p>\r\n      </nav>\r\n    </header>\r\n\r\n\r\n";
 },"useData":true});
 })();
