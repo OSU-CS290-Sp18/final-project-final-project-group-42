fdc687213d7feb079610c03d612580c4e69b40fc
austin <wilmotha@oregonstate.edu>
2018-05-28 17:45:51 -0700

    css files

--- public/templates/mixer.js
+++ public/templates/mixer.js
@@ -4,7 +4,7 @@ templates['mixer'] = template({"1":function(container,depth0,helpers,partials,da
     var stack1;
 
   return ((stack1 = container.invokePartial(partials.createButtonTemplate,depth0,{"name":"createButtonTemplate","data":data,"indent":"      ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + ((stack1 = container.invokePartial(partials.playListTemplate,depth0,{"name":"playListTemplate","data":data,"indent":"      ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + ((stack1 = container.invokePartial(partials.playlistBoxTemplate,depth0,{"name":"playlistBoxTemplate","data":data,"indent":"      ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
     + "    <!-- ";
 },"3":function(container,depth0,helpers,partials,data) {
     var stack1;
@@ -39,7 +39,7 @@ templates['mixer'] = template({"1":function(container,depth0,helpers,partials,da
 
   return "<!DOCTYPE html>\r\n<html>\r\n  <head>\r\n    <meta charset=\"uft-8\">\r\n    <title>Mixer - "
     + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</title>\r\n\r\n    <script> src=\"https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.11/handlebars.runtime.js\" defer></script>\r\n\r\n    <!-- <script src=\"/index.js\" charset=\"utf-8\" defer></script> -->\r\n  </head>\r\n\r\n"
+    + "</title>\r\n\r\n    <script> src=\"https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.11/handlebars.runtime.js\" defer></script>\r\n\r\n    <link rel=\"stylesheet\" href=\"/Css/buttonStyle.css\" media=\"screen\">\r\n    <link rel=\"stylesheet\" href=\"/Css/Header.css\" media=\"screen\">\r\n\r\n    <!-- <script src=\"/index.js\" charset=\"utf-8\" defer></script> -->\r\n\r\n  </head>\r\n\r\n"
     + ((stack1 = container.invokePartial(partials.headerTemplate,depth0,{"name":"headerTemplate","data":data,"indent":"  ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
     + "\r\n  <body>\r\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.home : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
--- public/templates/playlistBoxTemplate.js
+++ public/templates/playlistBoxTemplate.js
@@ -9,12 +9,12 @@ templates['playlistBoxTemplate'] = template({"1":function(container,depth0,helpe
     + alias4(((helper = (helper = helpers.songArtist || (depth0 != null ? depth0.songArtist : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"songArtist","hash":{},"data":data}) : helper)))
     + "\r\n        </p>\r\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing;
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
   return "<div class=\"playlistBox\">\r\n  <h4 class=\"playListName\">Good Songs</h4>\r\n  <!-- "
-    + container.escapeExpression(((helper = (helper = helpers.playListName || (depth0 != null ? depth0.playListName : depth0)) != null ? helper : alias2),(typeof helper === "function" ? helper.call(alias1,{"name":"playListName","hash":{},"data":data}) : helper)))
+    + container.escapeExpression(((helper = (helper = helpers.playListName || (depth0 != null ? depth0.playListName : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"playListName","hash":{},"data":data}) : helper)))
     + " -->\r\n    <div class=\"playListContentBox\">\r\n"
-    + ((stack1 = (helpers.each_upto || (depth0 && depth0.each_upto) || alias2).call(alias1,(depth0 != null ? depth0.songContent : depth0),2,{"name":"each_upto","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.songContent : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "    </div>\r\n</div>\r\n";
 },"useData":true});
 })();
