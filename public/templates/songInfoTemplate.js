fed17106d50522ad09190c0f426541082cf1d9f6
austin <wilmotha@oregonstate.edu>
2018-05-29 22:04:43 -0700

    fixed create button kinda

--- public/templates/songInfoTemplate.js
+++ public/templates/songInfoTemplate.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['songInfoTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class = \"add-song-info\">\r\n\r\n   <p class = \"Title\"> Title:\r\n   \r\n   <input type=\"text\" id=\"Song-input\" placeholder=\"Input here to search\">\r\n\r\n   </p>\r\n\r\n\r\n   <p class = \"Artist\"> Artist:\r\n\r\n   <input type=\"text\" id=\"Song-input\" placeholder=\"Input here to search\">\r\n\r\n   </p>\r\n\r\n\r\n  <button type=\"button\" id=\"Add-Song\">Add Song</button>\r\n\r\n</div>\r\n";
+    return "<div class = \"add-song-info\">\n\n   <p class = \"Title\"> Title:\n   \n   <input type=\"text\" id=\"Song-input\" placeholder=\"Input here to search\">\n\n   </p>\n\n\n   <p class = \"Artist\"> Artist:\n\n   <input type=\"text\" id=\"Song-input\" placeholder=\"Input here to search\">\n\n   </p>\n\n\n  <button type=\"button\" id=\"Add-Song\">Add Song</button>\n\n</div>\n";
 },"useData":true});
 })();
