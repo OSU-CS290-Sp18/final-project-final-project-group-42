15ca76e23c358bb69c04275ab2f0149146c3cc4f
austin <wilmotha@oregonstate.edu>
2018-06-03 23:39:03 -0700

    not much

--- public/templates/mixerPlaylistView.js
+++ public/templates/mixerPlaylistView.js
@@ -9,7 +9,7 @@ templates['mixerPlaylistView'] = template({"1":function(container,depth0,helpers
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
-  return "<h2 class='mixT=-title'>"
+  return "<h2 class='mixTitle'>"
     + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h2>\r\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.playlist : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
