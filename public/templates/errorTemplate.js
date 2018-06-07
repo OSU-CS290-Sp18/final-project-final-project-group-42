1c6d48bbf7acd3e1eac0fdeb9f3d014f4343e5af
Evdemonm <32214874+Evdemonm@users.noreply.github.com>
2018-06-07 10:46:32 -0700

    Stuff

--- public/templates/errorTemplate.js
+++ public/templates/errorTemplate.js
@@ -1,6 +1,10 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['errorTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+<<<<<<< HEAD
     return "\r\n\r\n    <main class=\"404\">\r\n    \r\n    <h1 class =\"404\"> Error Requested Page not Found </h1>\r\n    \r\n    </main>\r\n\r\n\r\n\r\n\r\n";
+=======
+    return "\n\n    <main class=\"404\">\n\n    <h1 class =\"404\"> Error 404: Requested Page not Found </h1>\n\n    </main>\n";
+>>>>>>> 2a8b5fc3143e197f08c2ced76384496951e237ff
 },"useData":true});
 })();
