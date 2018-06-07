934b5abd6decc5b99c51ea6ad74e567df27b6788
galatiaa <38193134+galatiaa@users.noreply.github.com>
2018-06-06 21:42:07 -0700

    Searching works, active search code gray for now

--- public/mixerMainFunction.js
+++ public/mixerMainFunction.js
@@ -4,6 +4,9 @@ var s2 = [];
 var songData = [];
 var tempData = [];
 var newName;
+var allPlaylists = [];
+var searchButton = document.getElementById('Mixer-Search');
+var searchInput = document.getElementById('navbar-search-input');
 
 //mixes the playlists
 function mixer(mixLevel, s1, s2){
@@ -144,7 +147,54 @@ function savePlaylists(event){
   });
 }
 
+// function insertNewPlaylist(name){
+//   var params = {playListName: name};
+//   var playListTemplate = Handlebars.templates.playListTemplate(params);
+//   var item = document.querySelector('.playlistBoxContainer');
+//   item.insertAdjacentHTML('beforeend', playListTemplate);
+// }
+//
+// function playlistMatchesQuery(playlist, query){
+//   if(!query){
+//     return true;
+//   }
+//
+//   query = query.trim().toLowerCase();
+//   return(playlist.playListName).toLowerCase().indexOf(query)>=0;
+// }
+//
+// function parsePlaylists(playlistElem){
+//   var playlist = {};
+//
+//   var playlistTitle = playlistElem.querySelector('.playListName a');
+//   playlist.playListName = playlistTitle.textContent.trim();
+//
+//   return playlist;
+// }
+//
+// function searchUpdate(){
+//   var query = document.getElementById('navbar-search-input').value;
+//
+//   var playlistContainer = document.querySelector('.playlistBoxContainer');
+//   if(playlistContainer){
+//     while(playlistContainer.lastChild){
+//       playlistContainer.removeChild(playlistContainer.lastChild);
+//     }
+//   }
+//
+//   allPlayLists.forEach(function (playlist){
+//     if(playlistMatchesQuery(playlist, query)){
+//         insertNewPlaylist(playlist.playListName);
+//     }
+//   });
+//
+// }
+
 window.addEventListener('DOMContentLoaded', function(){
+  // var playlistCollection = document.getElementsByClassName('playListName');
+  // for(var i = 0; i<playlistCollection.length; i++){
+  //   allPlaylists.push(parsePlaylists(playlistCollection[i]));
+  // }
   var addButtonLeft = document.getElementsByClassName('add-song-left');
   console.log(addButtonLeft);
   if(addButtonLeft.length != 0) {
@@ -159,5 +209,28 @@ window.addEventListener('DOMContentLoaded', function(){
   var mixButton = document.getElementById('mix-button');
   if(mixButton){
     mixButton.addEventListener('click', mixPlaylists);
+  }
+  // var searchButton = document.getElementById('Mixer-Search');
+  // if(searchButton){
+  //   searchButton.addEventListener('click', searchUpdate);
+  // }
+  // var searchInput = document.getElementById('navbar-search-input');
+  // if(searchInput){
+  //   searchInput.addEventListener('input', searchUpdate);
+  // }
+});
+
+searchButton.addEventListener('click', function(){
+  var i, filter, a;
+  filter = searchInput.value.toUpperCase();
+  var playlists = document.querySelectorAll('.playlistBoxContainer');
+  for(i = 0; i<playlists.length; i++){
+    a = playlists[i].getElementsByTagName("a")[0];
+    if(a.innerHTML.toUpperCase().indexOf(filter)>-1){
+      playlists[i].style.display = "";
+    }
+    else{
+      playlists[i].style.display = "none";
+    }
   }
 });
