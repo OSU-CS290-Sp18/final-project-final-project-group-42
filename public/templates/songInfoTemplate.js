7de116cf6075167fad9d1210b956f277ada0bb3e
Evdemonm <32214874+Evdemonm@users.noreply.github.com>
2018-06-11 22:12:37 -0700

    dumbo

--- public/templates/songInfoTemplate.js
+++ public/templates/songInfoTemplate.js
@@ -5,15 +5,9 @@ templates['songInfoTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":funct
 
   return "<div class = \"add-song-info\">\r\n   <p class = \"Title\"> Title:\r\n     <input type=\"text\" class=\"Song-input title"
     + alias4(((helper = (helper = helpers.side || (depth0 != null ? depth0.side : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"side","hash":{},"data":data}) : helper)))
-<<<<<<< HEAD
-    + "\" placeholder=\"Input here to search\">\r\n   </p>\r\n\r\n   <p class = \"Artist\"> Artist:\r\n     <input type=\"text\" class=\"Song-input artist"
+    + "\" placeholder=\"Song title\">\r\n   </p>\r\n\r\n   <p class = \"Artist\"> Artist:\r\n     <input type=\"text\" class=\"Song-input artist"
     + alias4(((helper = (helper = helpers.side || (depth0 != null ? depth0.side : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"side","hash":{},"data":data}) : helper)))
-    + "\" placeholder=\"Input here to search\">\r\n   </p>\r\n\r\n  <button type=\"button\" class=\"add-song add-song-"
-=======
-    + "\" placeholder=\"Song title\">\n   </p>\n\n   <p class = \"Artist\"> Artist:\n     <input type=\"text\" class=\"Song-input artist"
-    + alias4(((helper = (helper = helpers.side || (depth0 != null ? depth0.side : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"side","hash":{},"data":data}) : helper)))
-    + "\" placeholder=\"Artist Name\">\n   </p>\n\n  <button type=\"button\" class=\"add-song add-song-"
->>>>>>> f75253ae872989e08ad07a2047cebab26c5227f7
+    + "\" placeholder=\"Artist Name\">\r\n   </p>\r\n\r\n  <button type=\"button\" class=\"add-song add-song-"
     + alias4(((helper = (helper = helpers.side || (depth0 != null ? depth0.side : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"side","hash":{},"data":data}) : helper)))
     + "\">Add Song</button>\r\n</div>\r\n";
 },"useData":true});
