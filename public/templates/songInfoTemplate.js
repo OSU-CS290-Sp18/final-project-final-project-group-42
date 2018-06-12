f0bcdf7eff3625789fcdc1cb1fb7d0d51c6f95ab
Evdemonm <32214874+Evdemonm@users.noreply.github.com>
2018-06-12 04:20:19 -0700

    Final Fix + 404 Page CSS
    done

--- public/templates/songInfoTemplate.js
+++ public/templates/songInfoTemplate.js
@@ -5,9 +5,9 @@ templates['songInfoTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":funct
 
   return "<div class = \"add-song-info\">\r\n   <p class = \"Title\"> Title:\r\n     <input type=\"text\" class=\"Song-input title"
     + alias4(((helper = (helper = helpers.side || (depth0 != null ? depth0.side : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"side","hash":{},"data":data}) : helper)))
-    + "\" placeholder=\"Song title\">\r\n   </p>\r\n\r\n   <p class = \"Artist\"> Artist:\r\n     <input type=\"text\" class=\"Song-input artist"
+    + "\" maxLength=\"33\" placeholder=\"Song title\">\r\n   </p>\r\n\r\n   <p class = \"Artist\"> Artist:\r\n     <input type=\"text\" class=\"Song-input artist"
     + alias4(((helper = (helper = helpers.side || (depth0 != null ? depth0.side : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"side","hash":{},"data":data}) : helper)))
-    + "\" placeholder=\"Artist Name\">\r\n   </p>\r\n\r\n  <button type=\"button\" class=\"add-song add-song-"
+    + "\" maxLength=\"18\" placeholder=\"Artist Name\">\r\n   </p>\r\n\r\n  <button type=\"button\" class=\"add-song add-song-"
     + alias4(((helper = (helper = helpers.side || (depth0 != null ? depth0.side : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"side","hash":{},"data":data}) : helper)))
     + "\">Add Song</button>\r\n</div>\r\n";
 },"useData":true});
