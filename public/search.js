f4b825635eaabd7d25c923b7e6a25a463dba8e9d
austin <wilmotha@oregonstate.edu>
2018-06-11 23:41:36 -0700

    search updates on change and main js is cleaned up

--- public/search.js
+++ public/search.js
@@ -1,58 +1,74 @@
-
-function insertNewPlaylist(name){
-  var params = {playListName: name};
-  var playListTemplate = Handlebars.templates.playListTemplate(params);
-  var item = document.querySelector('.playlistBoxContainer');
-  item.insertAdjacentHTML('beforeend', playListTemplate);
-}
-
-function playlistMatchesQuery(playlist, query){
-  if(!query){
-    return true;
-  }
-
-  query = query.trim().toLowerCase();
-  return(playlist.playListName).toLowerCase().indexOf(query)>=0;
-}
-
-function parsePlaylists(playlistElem){
-  var playlist = {};
-
-  var playlistTitle = playlistElem.querySelector('.playListName a');
-  playlist.playListName = playlistTitle.textContent.trim();
-
-  return playlist;
-}
-
-function searchUpdate(){
-  var query = document.getElementById('navbar-search-input').value;
-
-  var playlistContainer = document.querySelector('.playlistBoxContainer');
-  if(playlistContainer){
-    while(playlistContainer.lastChild){
-      playlistContainer.removeChild(playlistContainer.lastChild);
-    }
-  }
-
-  allPlayLists.forEach(function (playlist){
-    if(playlistMatchesQuery(playlist, query)){
-        insertNewPlaylist(playlist.playListName);
-    }
-  });
-
-}
-
-searchButton.addEventListener('click', function(){
-  var i, filter, a;
-  filter = searchInput.value.toUpperCase();
-  var playlists = document.querySelectorAll('.playlistBoxContainer');
-  for(i = 0; i<playlists.length; i++){
-    a = playlists[i].getElementsByTagName("a")[0];
-    if(a.innerHTML.toUpperCase().indexOf(filter)>-1){
-      playlists[i].style.display = "";
-    }
-    else{
-      playlists[i].style.display = "none";
-    }
-  }
-});
+// function insertNewPlaylist(name){
+//   var params = {playListName: name};
+//   var playListTemplate = Handlebars.templates.playListTemplate(params);
+//   var item = document.querySelector('.playlistBoxContainer');
+//   item.insertAdjacentHTML('beforeend', playListTemplate);
+// }
+//
+// function playlistMatchesQuery(playlist, query){
+//   if(!query){
+//     return true;
+//   }
+//
+//   query = query.trim().toLowerCase();
+//   return(playlist.playListName).toLowerCase().indexOf(query)>=0;
+// }
+//
+// function parsePlaylists(playlistElem){
+//   var playlist = {};
+//
+//   var playlistTitle = playlistElem.querySelector('.playListName a');
+//   playlist.playListName = playlistTitle.textContent.trim();
+//
+//   return playlist;
+// }
+//
+// function searchUpdate(){
+//   var query = document.getElementById('navbar-search-input').value;
+//
+//   var playlistContainer = document.querySelector('.playlistBoxContainer');
+//   if(playlistContainer){
+//     while(playlistContainer.lastChild){
+//       playlistContainer.removeChild(playlistContainer.lastChild);
+//     }
+//   }
+//
+//   allPlayLists.forEach(function (playlist){
+//     if(playlistMatchesQuery(playlist, query)){
+//         insertNewPlaylist(playlist.playListName);
+//     }
+//   });
+// }
+//
+// searchButton.addEventListener('click', function(){
+//   var i, filter, a;
+//   filter = searchInput.value.toUpperCase();
+//   var playlists = document.querySelectorAll('.playlistBoxContainer');
+//   for(i = 0; i<playlists.length; i++){
+//     a = playlists[i].getElementsByTagName("a")[0];
+//     console.log(a);
+//     console.log(a.innerHTML.toUpperCase().indexOf(filter));
+//     if(a.innerHTML.toUpperCase().indexOf(filter)>-1){
+//       playlists[i].style.display = "";
+//     } else{
+//       playlists[i].style.display = "none";
+//     }
+//   }
+// });
+//
+// function searchBar (event) {
+//   var search = event.currentTarget.value.toLowerCase();
+//   var playlists = document.querySelectorAll('.playlistBoxContainer');
+//   var title = playlists[i].getElementsByTagName("a")[0];
+//   for(var i = 0; i < playlists.length; i++) {
+//     var title = playlists[i].getElementsByTagName("a")[0];
+//     if((title.textContent.toLowerCase().indexOf(search) >= 0)){
+//       playlists[i]classList.remove('hidden');
+//     }else {
+//       playlists[i]classList.classList.add('hidden');
+//     }
+//   }
+// }
+//
+// var search = document.getElementById('navbar-search-input');
+// search.addEventListener('input', searchBar);
--- public/templates/playListTemplate.js
+++ public/templates/playListTemplate.js
@@ -3,24 +3,18 @@
 templates['playListTemplate'] = template({"1":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "      <li>"
+  return "      <div class=\"title-list\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + " - "
+    + "</div>\n      <div class=\"artist-list\">"
     + alias4(((helper = (helper = helpers.artist || (depth0 != null ? depth0.artist : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"artist","hash":{},"data":data}) : helper)))
-    + "</li>\r\n";
+    + "</div>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
-<<<<<<< HEAD
   return "\n<div class = \"playListBox\">\n  <div class=\"playlist-name\">\n    "
     + container.escapeExpression(((helper = (helper = helpers.playListName || (depth0 != null ? depth0.playListName : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"playListName","hash":{},"data":data}) : helper)))
-    + "\n\n  </div>\n    <ol class = \"playLists\">\n"
-=======
-  return "<div class=\"playlist-name\">\r\n  "
-    + container.escapeExpression(((helper = (helper = helpers.playListName || (depth0 != null ? depth0.playListName : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"playListName","hash":{},"data":data}) : helper)))
-    + "\r\n</div><div class = \"playListBox\">\r\n  <ol class = \"playLists\">\r\n"
->>>>>>> 7de116cf6075167fad9d1210b956f277ada0bb3e
+    + "\n  </div>\n    <div class = \"playLists\">\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.list : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "  </ol>\r\n</div>\r\n";
+    + "  </div>\n</div>\n";
 },"useData":true});
 })();
